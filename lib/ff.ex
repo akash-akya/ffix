db00bfc0b347b8fbdd7164a0d15ea1a9f6dfac56
akash-akya <akashh246@gmail.com>
2024-07-06 22:36:35 +0530

    Initial Commit

---
+++ lib/ff.ex
@@ -0,0 +1,18 @@
+defmodule Ff do
+  @moduledoc """
+  Documentation for `Ff`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ff.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
